0c1ab8414e92bb736e3512075f7eca65026c248a
jackaroniandcheese <johnjosephoneill2003@gmail.com>
2026-04-27 20:10:47 -0400

    Added inline markdown converter

---
+++ src/split_nodes_delimiter.py
@@ -0,0 +1,20 @@
+from textnode import TextType, TextNode
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        if node.text.count(delimiter) % 2 != 0:
+            raise Exception("invalid markdown syntax")
+        split_text = node.text.split(delimiter)
+        
+        for i in range(0, len(split_text)):
+            if i % 2 == 0:
+                new_node = TextNode(split_text[i], TextType.TEXT)
+            else:
+                new_node = TextNode(split_text[i], text_type)
+            new_nodes.append(new_node)
+        
+    return new_nodes
---
+++ src/test_split_nodes_delimiter.py
@@ -0,0 +1,43 @@
+import unittest
+from split_nodes_delimiter import split_nodes_delimiter
+from textnode import TextNode, TextType
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+   # unittest.util._MAX_LENGTH = 999999999    
+
+    def test_text(self):
+        node = TextNode("This is simply raw text", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "_", TextType.TEXT)
+        self.assertEqual(
+                f"{new_nodes}",
+                "[TextNode(This is simply raw text, text, None)]"
+                )
+
+    def test_bold(self):
+        node = TextNode("This is text with a **bolded phrase** in the middle", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(
+                f"{new_nodes}",
+                "[TextNode(This is text with a , text, None), TextNode(bolded phrase, bold, None), TextNode( in the middle, text, None)]"
+                )
+
+    def test_italic(self):
+        node = TextNode("This is text with an _italicized phrase_ in the middle", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        self.assertEqual(
+                f"{new_nodes}",
+                "[TextNode(This is text with an , text, None), TextNode(italicized phrase, italic, None), TextNode( in the middle, text, None)]"
+                )
+
+    def test_code(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(
+                f"{new_nodes}",
+                "[TextNode(This is text with a , text, None), TextNode(code block, code, None), TextNode( word, text, None)]"
+                )
+
+    def test_invalid_syntax(self):
+        node = TextNode("This is text that is **incorrectly** using **bold lettering", TextType.TEXT)
+        with self.assertRaises(Exception):
+            split_nodes_delimiter([node], "**", TextType.BOLD)
